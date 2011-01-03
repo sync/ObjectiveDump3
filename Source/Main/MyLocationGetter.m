a90cef3853546e3630afd48ec7c9ec7a01a73bf6
Anthony Mittaz <sync@me.com>
2011-01-03 16:44:13 +1000

    fixed notification name on location getter

--- Source/Main/MyLocationGetter.m
+++ Source/Main/MyLocationGetter.m
@@ -66,7 +66,7 @@
 	if ([error code] == kCLErrorDenied) {
 		// should stop looking for coordinates
 		[self.locationManager stopUpdatingLocation];
-		[[NSNotificationCenter defaultCenter] postNotificationName:ShouldStopGPSLocationFix object:nil userInfo:nil];
+		[[NSNotificationCenter defaultCenter] postNotificationName:GPSLocationDidStop object:nil userInfo:nil];
 	}
 }
 
